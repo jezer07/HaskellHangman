187f4127c89dd0987e85930f7bfa7ab9b7865b55
jezer_crespo <jezercrespo@gmail.com>
2023-02-03 14:52:34 +0000

    A7 done

--- solutions/A7.hs
+++ solutions/A7.hs
@@ -9,17 +9,18 @@ import System.Directory ( doesFileExist )
 -- *** A7: Functors & Applicatives *** --
 
 -- Q#01
-
-getUpperChar = undefined
+getUpperChar:: IO String ->  IO String
+getUpperChar x = map toUpper <$> x
 
 -- Q#02
-
-_DICT_ = undefined
+_DICT_ :: IO [String]
+_DICT_ = lines <$> readFile _DICT_FILE_
 
 -- Q#03
-
-makeGameIfValid = undefined
+makeGameIfValid:: Either GameException Secret -> Either GameException Game
+makeGameIfValid (Left x)= Left x
+makeGameIfValid y = makeGame <$> y
 
 -- Q#04
-
-getDict = undefined
+getDict:: IO (Maybe Dictionary)
+getDict = toMaybe <$>  doesFileExist _DICT_FILE_ <*> _DICT_
